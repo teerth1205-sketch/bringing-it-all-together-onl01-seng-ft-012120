93e6672cfb677c477f7b605ba19ea385ca16227a
Teerth Patel <teerth1205@gmail.com>
2020-02-23 22:52:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,7 +58,16 @@ def self.create(name:, breed:)
  dog
 end 
   
-def self.find_or_create_by
+def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end
   
 
 end 
