97c19481dbf62e6d174e1ec51bd4afed7f973dfd
Teerth Patel <teerth1205@gmail.com>
2020-02-23 23:15:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,7 @@ def self.find_or_create_by(name:, breed:)
 def self.find_by_id(id)
   sql = "SELECT id FROM dogs WHERE id = ?"
 dog = DB[:conn].execute(sql, id)
-  Dog.new_from_db(dog)
+  
 end 
 
 end 
