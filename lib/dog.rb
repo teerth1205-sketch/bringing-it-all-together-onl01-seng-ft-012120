312262887e27e2fa343efa7f9f029f2620f02639
Teerth Patel <teerth1205@gmail.com>
2020-02-23 22:02:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,5 +6,14 @@ def initialize(id: nil, name:, breed:)
   @name = name 
   @breed = breed
 end   
+
+def self.create_table
+  sql = "CREATE TABLE dogs (
+  id INTEGER PRIMARY KEY, 
+  name TEXT,
+  breed TEXT
+  )"
+  DB[:conn].execute(sql)
+end 
   
 end 
