61aec3cf6afcbc8ae9a636bbfb5bb911f4b33a17
Teerth Patel <teerth1205@gmail.com>
2020-02-23 22:32:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,7 +30,7 @@ def self.new_from_db(row)
   id = row[0]  
   name = row[1]  
   breed = row[2] 
-  Dog.new(id: id, name: name, breed: breed)
+  self.new(id: id, name: name, breed: breed)
 end 
 
 def self.find_by_name(name)
