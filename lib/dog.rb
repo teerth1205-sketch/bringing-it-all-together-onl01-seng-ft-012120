22140d0099315efda4e8fe445c2744a1bce786a5
Teerth Patel <teerth1205@gmail.com>
2020-02-23 22:25:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,7 +41,7 @@ def self.find_by_name(name)
 end 
   
 def update
-  sql = "UPDATE dogs SET name = ?, breed = ?, WHERE id = ?"
+  sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
   DB[:conn].execute(sql, self.name, self.breed, self.id)
   
 end 
